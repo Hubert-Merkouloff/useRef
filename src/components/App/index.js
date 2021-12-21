00e963386917e31752dc4ebd7f46dfda0e8f9312
Hubert-Merkouloff <hubert.merkouloff@gmail.com>
2021-12-21 22:34:12 +0100

    simple use of useRef

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.scss';
 
+
+// 1. I import the hook useRef
+// 2. I create an objet with the property current
+// 3. I link the previously created object with an element  
+// 4. I can access my element with object.current
+
+
+//!1
+import { useRef } from 'react';
+
+
+
+
 function App() {
+
+  //! 2
+  const buttonRef = useRef();
+
+  const handleClick = () => {
+
+    //!4
+    const element = buttonRef.current.focus();
+
+    console.log(buttonRef.current);
+ 
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button className="button" onClick={handleClick}>
+        main button
+      </button>
+
+      //!3
+      <button className="button" ref={buttonRef}>
+        secondary button
+      </button>
+
+
+      
+
+
+
+     
     </div>
   );
 }
